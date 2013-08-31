23804a5e0beb8c74e770b58aa58bbf1806390b07
Josh Rivers <jrivers@eidpassport.com>
2013-08-30 21:58:41 -0700

    Feature complete. Encodes the strings.

--- CSharpStringEscaper/CSharpStringEscaper/Controllers/EscapeController.cs
+++ CSharpStringEscaper/CSharpStringEscaper/Controllers/EscapeController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,8 +13,36 @@ namespace CSharpStringEscaper.Controllers
     {
         public ActionResult Index()
         {
+            this.ViewBag.multilineChecked = true;
             return View();
         }
 
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(FormCollection form)
+        {
+            bool multilineChecked = !string.IsNullOrEmpty(form["useMultiline"]);
+            this.ViewBag.input = form["unescapedCodeInput"];
+            this.ViewBag.result = ToLiteral(form["unescapedCodeInput"], multilineChecked);
+            this.ViewBag.multilineChecked = multilineChecked;
+            return View();
+        }
+
+        private static string ToLiteral(string input, bool multiline)
+        {
+            using (var writer = new StringWriter())
+            {
+                using (var provider = CodeDomProvider.CreateProvider("CSharp"))
+                {
+                    provider.GenerateCodeFromExpression(new CodePrimitiveExpression(input), writer, new CodeGeneratorOptions { IndentString = "\t" });
+                    var literal = writer.ToString();
+                    if (!multiline)
+                    {
+                        literal = literal.Replace(string.Format("\" +{0}\t\"", Environment.NewLine), "");
+                    }
+                    return literal;
+                }
+            }
+        }
     }
 }
